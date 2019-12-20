c79cdaaed7fd59c70fbb8a455f604996b28eb1eb
Alexandra Trofimova <alazingara@inbox.ru>
2019-12-20 23:54:46 +0300

    Отладка программы в IDEA[#198642]

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -1,14 +1,10 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
-        double h = p / 2 / (k+1);
-        double L = h * k;
-        return L * h;
-    }
-
-    public static void main(String[] args) {
-        double result1 = square(6, 2);
-        System.out.println(" p = 6, k = 2, s = " + result1);
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SqMaxTest {
+    @Test
+    public void whenFirstMax() {
+        SqMax check = new SqMax();
+        int result = check.max(6, 4, 2, 1);
+        assertThat(result, is(6));
+    }
+    @Test
+    public void whenSecondMax() {
+        SqMax check = new SqMax();
+        int result = check.max(1, 7, 2, 6);
+        assertThat(result, is(7));
+    }
+    @Test
+    public void whenThirdMax() {
+        SqMax check = new SqMax();
+        int result = check.max(1, 4, 8, 6);
+        assertThat(result, is(8));
+    }
+    @Test
+    public void whenForthMax() {
+        SqMax check = new SqMax();
+        int result = check.max(1, 4, 2, 5);
+        assertThat(result, is(5));
+    }
+    @Test
+    public void whenFourEquals() {
+        SqMax check = new SqMax();
+        int result = check.max(2, 2, 2, 2);
+        assertThat(result, is(2));
+    }
+}
