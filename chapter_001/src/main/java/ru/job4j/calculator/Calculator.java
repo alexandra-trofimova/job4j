af2b76c880a3c7ca961c6766164aa4aea724fbee
Alexandra Trofimova <alazingara@inbox.ru>
2019-12-29 22:52:54 +0300

    JavaDoc. Code convention[#198649]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,22 +1,32 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Класс для вычисления арифметических операций + - * /
+ * @author Alexandra Trofimova
+ * @since 03.12.2019
+ * @version 1
+ */
 public class Calculator {
-    public static void add(int first, int second) {
+        public static void add(int first, int second) {
         int result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
-    public static void div(int first, int second) {
+        public static void div(int first, int second) {
         int result =  first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
-    public static void multiply(int first, int second) {
+        public static void multiply(int first, int second) {
         int result =  first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
-    public static void subtrack(int first, int second) {
+        public static void subtrack(int first, int second) {
         int result =  first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
+    /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         add(1, 1);
         div (4,2);
