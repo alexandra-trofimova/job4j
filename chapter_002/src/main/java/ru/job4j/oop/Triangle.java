503b457ff6ba1f832e21c07867febd61908fe4ef
Alexandra Trofimova <alazingara@inbox.ru>
2020-02-16 01:03:13 +0300

    Рефакторинг - Площадь треугольника.[#198782]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
