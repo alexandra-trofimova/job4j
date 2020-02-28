19cbcf40e4b8c13e63c741ddad6a0e3b1b8f9ff9
Alexandra Trofimova <alazingara@inbox.ru>
2020-02-28 11:14:44 +0300

    Перегрузка конструктора.[#198799]

--- chapter_002/src/main/java/ru/job4j/tracker/Animal.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Animal.java
@@ -1,13 +1,20 @@
 package ru.job4j.tracker;
 
 public class Animal {
+    private String name;
 
     public Animal() {
         super();
         System.out.println("Animal");
     }
 
+    public Animal(String name) {
+        this.name = name;
+        System.out.println("Animal " + name);
+    }
+
     public static void main(String[] args) {
-        Animal animal = new Animal();
+        Animal animal1 = new Animal();
+        Animal animal2 = new Animal("Vasya");
     }
 }
