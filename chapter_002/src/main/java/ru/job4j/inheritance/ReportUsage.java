2427a0532ba1d9a1e3827cbd12ca24cb303a4d5e
Alexandra Trofimova <alazingara@inbox.ru>
2020-03-05 11:34:41 +0300

    Переопределение[#198800]

--- chapter_002/src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -2,6 +2,7 @@ package ru.job4j.inheritance;
 
 public class ReportUsage {
     public static void main(String[] args) {
+
         JSONReport report = new JSONReport();
         String text = report.generate("Report's name", "Report's body");
         System.out.println(text);
