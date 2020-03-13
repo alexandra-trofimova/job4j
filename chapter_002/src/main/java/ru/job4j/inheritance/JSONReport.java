23044cdf1e3d2ced282d810606656d3dd80c474e
Alexandra Trofimova <alazingara@inbox.ru>
2020-03-13 19:01:40 +0300

    Переопределение[#198800]

--- chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,7 +3,7 @@ package ru.job4j.inheritance;
 /**
  * Class Класс для создания отчета в формате JSON
  * @author Alexandra Trofimova
- * @since 10.03.2020
+ * @since 13.03.2020
  * @version 1
  */
 
