22e7f0807b93d3603de961bd5c5a5d4d370ba88a
Alexandra Trofimova <alazingara@inbox.ru>
2020-03-14 20:46:28 +0300

    Аннотация @Override[#198801]

--- chapter_002/src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -13,5 +13,6 @@ public class ReportUsage {
         JSONReport report = new JSONReport();
         String text = report.generate("Report's name", "Report's body");
         System.out.println(text);
+
     }
 }
